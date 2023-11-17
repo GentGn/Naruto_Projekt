5c0954ff51dca86292ced1d9410e1de27d44a0bd
gent ndoci <artstudio.101@gmx.at>
2023-11-17 01:07:42 +0100

    Kommentare_beigefuegt

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,10 @@
-from flask import Flask
-from website.routes import dragonball_bp
+from flask import Flask  # Import der Flask-Klasse
+from website.routes import dragonball_bp  # Import des Blueprint für Dragonball
 
 def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(dragonball_bp)
-    return app
+  
+    app = Flask(__name__)  # Erstellt eine Flask-Instanz
+
+    app.register_blueprint(dragonball_bp)  # Registriert den Dragonball Blueprint
+
+    return app  # Gibt die Flask-App zurück
--- website/routes.py
+++ website/routes.py
@@ -1,33 +1,36 @@
 from flask import Blueprint, jsonify, request
 import requests
 
+# Erstellung eines Blueprint-Objekts mit dem Namen 'dragonball'
 dragonball_bp = Blueprint('dragonball', __name__)
 
+# Route für alle Charaktere
 @dragonball_bp.route('/api/character/')
 def get_characters():
+    # URL zur API
     api_url = 'https://dbapidb.herokuapp.com/api/character/'
     response = requests.get(api_url)
     
+    # Überprüfung der Antwort
     if response.status_code == 200:
         characters_data = response.json()
-        return jsonify(characters_data)
+        return jsonify(characters_data)  # Rückgabe der Daten im JSON-Format
     else:
         return 'Fehler beim Abrufen der Daten.'
 
+# Route für einen bestimmten Charakter anhand des Namens
 @dragonball_bp.route('/api/character/<name>')
 def get_single_character(name):
-    name = name.replace(" ", "_")
+    name = name.replace(" ", "_")  # Formatierung des Namens für die API-Anfrage
     api_url = f'https://dbapidb.herokuapp.com/api/character/{name}'
     
     response = requests.get(api_url)
     
+    # Überprüfung der Antwort und Rückgabe der Daten oder Fehlermeldung
     if response.status_code == 200:
         character_data = response.json()
         return jsonify(character_data)
     else:
         return "Charakter wurde nicht gefunden."
-
-   
-
 
         
